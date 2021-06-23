fe961eb9b9dcdba1e9efcf8d5db70b1dc1acbd14
Rubel Sarker <rubel_sarker@dellteam.com>
2021-06-23 16:18:29 +0100

    change todo logic

--- todo.js
+++ todo.js
@@ -3,39 +3,80 @@ const todoInput = document.querySelector('#todo-input');
 
 const addToDo = () => {
 	const todoInput = document.querySelector('#todo-input');
+	const container = document.querySelector('#todo-container');
+
 	if (todoInput.value) {
-		createToDoHtml(todoInput.value);
+		let createdTodo = createToDoHtml(todoInput.value, container);
+		container.prepend(createdTodo);
 		todoInput.value = '';
 	} else {
 		alert('please enter your do to..!!!');
 	}
 };
 
-const updateToDo = () => {
-	alert('clicked');
-	var editInput = document.createElement('input'); //text
-	var editButton = document.createElement('button'); //edit button
+function editTodo(input) {
+	let editBtn = create(
+		'button',
+		'btn btn-outline-secondary bg-success text-white'
+	);
+	editBtn.innerHTML = 'EDIT';
+	editBtn.setAttribute('type', 'button');
+	editBtn.addEventListener('click', () => {
+		if (editBtn.innerHTML === 'EDIT') {
+			editBtn.innerHTML = 'UPDATE';
+			input.removeAttribute('readonly');
+		} else if (editBtn.innerHTML === 'UPDATE') {
+			editBtn.innerHTML = 'EDIT';
+			input.setAttribute('readonly', '');
+		}
+	});
+	return editBtn;
+}
+
+function deleteTodo(container, todoDiv) {
+	let deleteBtn = create(
+		'button',
+		'btn btn-outline-secondary bg-danger text-white'
+	);
+	deleteBtn.innerHTML = 'DELETE';
+	deleteBtn.setAttribute('type', 'button');
+
+	deleteBtn.addEventListener('click', () => {
+		container.removeChild(todoDiv);
+	});
+	return deleteBtn;
+}
+
+const createToDoHtml = (userInput, container) => {
+	let todoDiv = document.createElement('div');
+	todoDiv.classList = 'pb-3 todo-item';
+
+	let inputGroupDiv = document.createElement('div');
+	inputGroupDiv.className = 'input-group';
+
+	let input = create('input', 'form-control');
+	input.setAttribute('readonly', '');
+	input.setAttribute('placeholder', 'Enter your edited task');
+	input.value = userInput;
+	inputGroupDiv.appendChild(input);
+
+	let editBtn = editTodo(input);
+	inputGroupDiv.appendChild(editBtn);
+
+	let deleteBtn = deleteTodo(container, todoDiv);
+	inputGroupDiv.appendChild(deleteBtn);
+
+	todoDiv.append(inputGroupDiv);
+
+	return todoDiv;
 };
 
-const deleteToDo = () => {
-	const delBtnn = document.getElementById('todolist');
-	delBtnn.remove();
-};
+function create(tag, className) {
+	let createTag = document.createElement(tag);
+	createTag.classList = className;
 
-const createToDoHtml = (userInput) => {
-	let newTodoHTML = `
-    <div class="pb-3 todo-item" id="todolist">
-        <div class="input-group">
-             <input type="text" readonly class="form-control "aria-label="Text input" value="${userInput}" >
-             <button class="btn btn-outline-secondary bg-success text-white" type="button" onclick="updateToDo()" id="edit">EDIT</button>
-             <button class="btn btn-outline-secondary bg-danger text-white" type="button" onclick="deleteToDo()" id="delete">DELETE</button>
-        </div>
-    </div>`;
-
-	let div = document.createElement('DIV');
-	div.innerHTML = newTodoHTML;
-	document.getElementById('todo-container').appendChild(div.children[0]);
-};
+	return createTag;
+}
 
 addBtn.addEventListener('click', addToDo);
 todoInput.addEventListener('keydown', (event) => {
